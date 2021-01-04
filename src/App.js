0915b0bbd99fab2fc64c575b8699a0003cc0a907
Sayan Datta <dattasayanbca@gmail.com>
2021-01-04 15:44:40 +0530

    deploy my digital clock

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App=()=>{
+
+  let currTime=new Date().toLocaleTimeString();
+  const [newTime,setTime]=new useState(currTime);
+  const updateTime=()=>{
+    let currTime=new Date().toLocaleTimeString();
+    setTime(currTime);
+  }
+  setInterval(updateTime,1000);
+  return(
+  <>
+  <h1>Digital Clock</h1>
+  <h2>{newTime}</h2>
+  </>)
 }
-
 export default App;
